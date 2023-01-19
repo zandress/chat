58b4577d07df26e787f402c19cb283b0ed0e45e4
Zach <74432623+zandress@users.noreply.github.com>
2023-01-19 13:22:11 -0700

    installing @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'zandress-chat-ebbb6',
+    appId: '1:340595076544:web:bf7abc809a3167768b2c25',
+    storageBucket: 'zandress-chat-ebbb6.appspot.com',
+    apiKey: 'AIzaSyCBugHc5DOkI3SakyuEmNBte54yUWJPB04',
+    authDomain: 'zandress-chat-ebbb6.firebaseapp.com',
+    messagingSenderId: '340595076544',
+    measurementId: 'G-6GD1YKHE0R',
+  },
   production: true
 };
