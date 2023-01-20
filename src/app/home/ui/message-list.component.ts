4e56b394f7e05771db8975a44ed323d42e077a34
Zach <74432623+zandress@users.noreply.github.com>
2023-01-20 15:52:57 -0700

    created the message list component

---
+++ src/app/home/ui/message-list.component.ts
@@ -0,0 +1,66 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  NgModule,
+} from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { Message } from '../../shared/interfaces/message';
+
+@Component({
+  selector: 'app-message-list',
+  template: `
+    <ion-list lines="none">
+      <ion-item *ngFor="let message of messages; trackBy: trackByFn">
+        <ion-avatar class="animate-in-primary">
+          <img
+            *ngIf="message.author"
+            src="https://avatars.dicebear.com/api/bottts/{{
+              message.author.split('@')[0]
+            }}.svg"
+          />
+        </ion-avatar>
+        <div class="chat-message animate-in-secondary">
+          <ion-note>{{ message.author }}</ion-note>
+          <p>{{ message.content }}</p>
+        </div>
+      </ion-item>
+    </ion-list>
+  `,
+  styles: [
+    `
+      .chat-message,
+      ion-avatar {
+        filter: drop-shadow(2px 4px 6px var(--ion-color-primary-shade));
+      }
+
+      .chat-message {
+        width: 100%;
+        padding: 10px;
+        border-radius: 10px;
+        margin: 10px 0;
+        background-color: var(--ion-color-light);
+
+        p {
+          margin: 5px 0;
+        }
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MessageListComponent {
+  @Input() messages!: Message[];
+
+  trackByFn(index: number, message: Message) {
+    return message.created;
+  }
+}
+
+@NgModule({
+  declarations: [MessageListComponent],
+  exports: [MessageListComponent],
+  imports: [CommonModule, IonicModule],
+})
+export class MessageListComponentModule {}
