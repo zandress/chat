6aa24911845406260c5271d32ef495997cd7de3c
Zach <74432623+zandress@users.noreply.github.com>
2023-01-23 13:28:35 -0700

    created a message input component

---
+++ src/app/home/ui/message-input.component.ts
@@ -0,0 +1,55 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  NgModule,
+  Output,
+} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-message-input',
+  template: `
+    <ion-toolbar color="primary">
+      <ion-textarea
+        [formControl]="messageControl"
+        placeholder="type message..."
+      ></ion-textarea>
+      <ion-buttons slot="primary">
+        <ion-button (click)="sendMessage()">
+          <ion-icon name="send" slot="icon-only"></ion-icon>
+        </ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+  `,
+  styles: [
+    `
+      ion-textarea {
+        font-size: 1.2em !important;
+        --padding-top: 25px;
+        --padding-start: 15px;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MessageInputComponent {
+  @Output() send = new EventEmitter<string>();
+
+  messageControl = new FormControl('');
+
+  sendMessage() {
+    if (this.messageControl.value) {
+      this.send.emit(this.messageControl.value);
+      this.messageControl.reset();
+    }
+  }
+}
+
+@NgModule({
+  declarations: [MessageInputComponent],
+  exports: [MessageInputComponent],
+  imports: [IonicModule, ReactiveFormsModule],
+})
+export class MessageInputComponentModule {}
