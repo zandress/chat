cc9811f2a070c3e8c85d2da3a066eec5b0cafa8d
Zach <74432623+zandress@users.noreply.github.com>
2023-01-23 11:59:26 -0700

    creating the store for messages array

---
+++ src/app/home/data-access/home.store.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { switchMap } from 'rxjs/operators';
+import { MessageService } from 'src/app/shared/data-access/message.service';
+import { Message } from 'src/app/shared/interfaces/message';
+
+export interface HomeState {
+  messages: Message[];
+}
+
+@Injectable()
+export class HomeStore extends ComponentStore<HomeState> {
+  message$ = this.select((state) => state.messages);
+
+  loadMessages = this.effect(($) =>
+    $.pipe(
+      switchMap(() =>
+        this.messageService.getMessages().pipe(
+          tapResponse(
+            (messages) => this.patchState({ messages }),
+            (err) => console.log(err)
+          )
+        )
+      )
+    )
+  );
+
+  constructor(private messageService: MessageService) {
+    super({ messages: [] });
+  }
+}
