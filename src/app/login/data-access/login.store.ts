7e59f12417e19e2b8b14c9d189922f67336f44a2
Zach <74432623+zandress@users.noreply.github.com>
2023-01-24 11:13:41 -0700

    created the login component store

---
+++ src/app/login/data-access/login.store.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { AuthService } from 'src/app/shared/data-access/auth.service';
+import { Credentials } from 'src/app/shared/interfaces/credentials';
+
+export type LoginStatus = 'pending' | 'authenticating' | 'success' | 'error';
+
+interface LoginState {
+  status: LoginStatus;
+  createModalIsOpen: boolean;
+}
+
+@Injectable()
+export class LoginStore extends ComponentStore<LoginState> {
+  status$ = this.select((state) => state.status);
+  createModalIsOpen$ = this.select((state) => state.createModalIsOpen);
+
+  login = this.effect((credentials$: Observable<Credentials>) =>
+    credentials$.pipe(
+      tap(() => this.patchState({ status: 'authenticating' })),
+      switchMap((credentials) =>
+        this.authService.login(credentials).pipe(
+          tapResponse(
+            (user) => {
+              this.patchState({ status: 'success' });
+              this.navCtrl.navigateRoot('/home');
+            },
+            (error) => this.patchState({ status: 'error' })
+          )
+        )
+      )
+    )
+  );
+
+  constructor(
+    private authService: AuthService,
+    private navCtrl: NavController
+  ) {
+    super({ status: 'pending', createModalIsOpen: false });
+  }
+
+  setCreateModalIsOpen(isOpen: boolean) {
+    this.patchState({ createModalIsOpen: isOpen });
+  }
+}
